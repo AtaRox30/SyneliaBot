c46fb2178188b2d2f8f55b4a81e71a50abf5aef9
AtaRox30 <altarox30@gmail.com>
2022-12-16 19:33:56 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,190 @@
+require('dotenv').config();
+const fs = require('fs');
+const { Client, GatewayIntentBits, EmbedBuilder, Presence, Collection, Interaction } = require('discord.js');
+const twitch = require('./twitch');
+const config = require('./config.json');
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
+});
+
+var worker = null;
+
+const thread = async () => {
+	const channel = await checkStream();
+	await checkVODS(channel);
+	await checkClips(channel);
+	const utcNow = new Date().toUTCString();
+	config["LAST_CHECKED"] = new Date(utcNow).toISOString();
+	fs.writeFileSync("./config.json", JSON.stringify(config, null, 4));
+};
+
+const checkStream = async () => {
+	const channel = await twitch.getChannel();
+	if(!config["IS_LIVE"] && channel.is_live)
+	{
+		//Streamer wasn't streaming the last time we checked, but is streaming now, so we send
+		notifyStream(channel);
+	}
+	config["IS_LIVE"] = channel.is_live;
+	fs.writeFileSync("./config.json", JSON.stringify(config, null, 4));
+	return channel;
+}
+
+const checkVODS = async (channel) => {
+	const aVods = await twitch.getVODS();
+	const aNotified = aVods.filter(v => new Date(v.published_at) > new Date(config["LAST_CHECKED"]));
+	if(aNotified.length)
+	{
+		//At least one video found, notify
+		notifyVods(channel, aNotified);
+	}
+}
+
+const checkClips = async (channel) => {
+	const aClips = await twitch.getClips();
+	const aNotified = aClips.filter(v => new Date(v.created_at) > new Date(config["LAST_CHECKED"]));
+	if(aNotified.length)
+	{
+		//At least one video found, notify
+		notifyClips(channel, aNotified);
+	}
+}
+
+const notifyStream = async (channel) => {
+	/*
+		channel {
+			display_name: string,
+			game_id: string,
+			game_name: string,
+			thumbnail_url: string,
+			title: string
+		}
+	*/
+	const guild = client.guilds.cache.get(config["DISCORD"]["GUILD_ID"]);
+	const channelDisc = guild.channels.cache.get(config["DISCORD"]["CHANNELS"]["ONLINE"]);
+	const exampleEmbed = new EmbedBuilder()
+		.setColor(0xB00514)
+		.setTitle(channel.display_name + " est en stream !")
+		.setURL('https://www.twitch.tv/syneliasan')
+		.setAuthor({ name: channel.display_name, iconURL: channel.thumbnail_url, url: 'https://www.twitch.tv/syneliasan' })
+		.setDescription(channel.title)
+		.setThumbnail(channel.thumbnail_url)
+
+	channelDisc.send({
+		content: '@everyone',
+		embeds: [exampleEmbed]
+	});
+}
+
+const notifyVods = async (channel, aVods) => {
+	aVods.forEach(async vod => {
+		/*
+			channel {
+				display_name: string,
+				game_id: string,
+				game_name: string,
+				thumbnail_url: string,
+				title: string
+			}
+
+			vod {
+				user_name: string,
+				title: string,
+				published_at: string,
+				url: string,
+				thumbnail_url: string,
+				description: string
+			}
+
+			game {
+				id: string,
+				name: string,
+				box_art_url: string,
+				igdb_id: string
+			}
+		*/
+		const game = await twitch.getGame(channel.game_id);
+		const thumbGame = game.box_art_url.replace("{width}", 285).replace("{height}", 380);
+		const thumbVideo = vod.thumbnail_url.replace("%{width}", 800).replace("%{height}", 450);
+		const guild = client.guilds.cache.get(config["DISCORD"]["GUILD_ID"]);
+		const channelDisc = guild.channels.cache.get(config["DISCORD"]["CHANNELS"]["VOD"]);
+		const exampleEmbed = new EmbedBuilder()
+			.setColor(0xB07705)
+			.setTitle(vod.title)
+			.setURL(vod.url)
+			.setAuthor({ name: vod.user_name, iconURL: channel.thumbnail_url, url: 'https://www.twitch.tv/syneliasan' })
+			.setDescription(vod.description.length ? vod.description : "Aucune description")
+			.setThumbnail(thumbGame)
+			.setImage(thumbVideo)
+
+		channelDisc.send({
+			content: '@everyone',
+			embeds: [exampleEmbed]
+		});
+	});
+}
+
+const notifyClips = (channel, aClips) => {
+	aClips.forEach(async clip => {
+		/*
+			channel {
+				display_name: string,
+				game_id: string,
+				game_name: string,
+				thumbnail_url: string,
+				title: string
+			}
+
+			clip {
+				url: string,
+				broadcaster_name: string,
+				creator_name: string,
+				creator_id: string,
+				video_id: string,
+				game_id: string,
+				title: string,
+				created_at: string,
+				thumbnail_url: string
+			}
+
+			game {
+				id: string,
+				name: string,
+				box_art_url: string,
+				igdb_id: string
+			}
+
+			creator {
+				id: string,
+				profile_image_url: string
+			}
+		*/
+
+		const game = await twitch.getGame(clip.game_id);
+		const creator = await twitch.getUser(clip.creator_id);
+		const thumbGame = game.box_art_url.replace("{width}", 285).replace("{height}", 380);
+		const thumbVideo = clip.thumbnail_url.replace("%{width}", 800).replace("%{height}", 450);
+		const guild = client.guilds.cache.get(config["DISCORD"]["GUILD_ID"]);
+		const channelDisc = guild.channels.cache.get(config["DISCORD"]["CHANNELS"]["CLIPS"]);
+		const exampleEmbed = new EmbedBuilder()
+			.setColor(0x4A9428)
+			.setTitle(clip.title)
+			.setURL(clip.url)
+			.setAuthor({ name: clip.creator_name, iconURL: creator.profile_image_url })
+			.setThumbnail(thumbGame)
+			.setImage(thumbVideo)
+
+		channelDisc.send({
+			content: '@everyone',
+			embeds: [exampleEmbed]
+		});
+	});
+}
+
+client.on("ready", async () => {
+    console.log("Discord bot ready");
+	worker = setInterval(thread, 10000);
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
---
+++ twitch.js
@@ -0,0 +1,203 @@
+const config = require("./config.json");
+const request = require("request");
+const fs = require('fs');
+
+const getToken = (callback) => {
+	const utcNow = new Date().toUTCString();
+	const utcDate = new Date(utcNow);
+	if(utcDate > new Date(config['TWITCH_EXPIRATION']))
+	{
+		const options = {
+			url: config.URL["GET_TOKEN"],
+			json: true,
+			body: {
+				client_id: process.env.TWITCH_CLIENT_ID,
+				client_secret: process.env.TWITCH_CLIENT_SECRET,
+				grant_type: 'client_credentials'
+			}
+		}
+	
+		request.post(options, (err, res, body) => {
+			if(err) return console.error(err);
+	
+			config['TWITCH_ACCESS_TOKEN'] = body.access_token;
+			const expireUTC = utcDate.setSeconds(utcDate.getSeconds() + body.expires_in);
+			config['TWITCH_EXPIRATION'] = new Date(expireUTC);
+			fs.writeFileSync("./config.json", JSON.stringify(config, null, 4));
+			callback(body.access_token);
+		})
+	}
+	else
+	{
+		callback(config['TWITCH_ACCESS_TOKEN']);
+	}
+}
+
+const getVODPage = (at, pagination) => {
+	const options = {
+		url: config.URL["GET_VIDEOS"] + '?user_id=' + config["BROADCASTER"]["ID"] + (pagination ? ('&after=' + pagination) : '') + '&sort=time',
+		json: true,
+		headers: {
+			'Client-Id': process.env.TWITCH_CLIENT_ID,
+			'Authorization': 'Bearer ' + at
+		}
+	}
+
+	return new Promise(function(resolve, reject) {
+		request.get(options, (err, res, body) => {
+			if (!err && res.statusCode === 200) {
+				resolve(body);
+			} else {
+				reject(err);
+			}
+		})
+	});
+}
+
+const getClipsPage = (at) => {
+	const options = {
+		url: config.URL["GET_CLIPS"] + '?broadcaster_id=' + config["BROADCASTER"]["ID"] + '&sort=time',
+		json: true,
+		headers: {
+			'Client-Id': process.env.TWITCH_CLIENT_ID,
+			'Authorization': 'Bearer ' + at
+		}
+	}
+
+	return new Promise(function(resolve, reject) {
+		request.get(options, (err, res, body) => {
+			if (!err && res.statusCode === 200) {
+				resolve(body);
+			} else {
+				reject(err);
+			}
+		})
+	});
+}
+
+const getStreamers = (at) => {
+	const options = {
+		url: config.URL["GET_CHANNELS"] + '?query=' + config["BROADCASTER"]["NAME"],
+		json: true,
+		headers: {
+			'Client-Id': process.env.TWITCH_CLIENT_ID,
+			'Authorization': 'Bearer ' + at
+		}
+	}
+
+	return new Promise(function(resolve, reject) {
+		request.get(options, (err, res, body) => {
+			if (!err && res.statusCode === 200) {
+				resolve(body);
+			} else {
+				reject(err);
+			}
+		})
+	});
+}
+
+const getGames = (at, id) => {
+	const options = {
+		url: config.URL["GET_GAMES"] + '?id=' + id,
+		json: true,
+		headers: {
+			'Client-Id': process.env.TWITCH_CLIENT_ID,
+			'Authorization': 'Bearer ' + at
+		}
+	}
+
+	return new Promise(function(resolve, reject) {
+		request.get(options, (err, res, body) => {
+			if (!err && res.statusCode === 200) {
+				resolve(body);
+			} else {
+				reject(err);
+			}
+		})
+	});
+}
+
+const getUsers = (at, id) => {
+	const options = {
+		url: config.URL["GET_USER_ID"] + '?id=' + id,
+		json: true,
+		headers: {
+			'Client-Id': process.env.TWITCH_CLIENT_ID,
+			'Authorization': 'Bearer ' + at
+		}
+	}
+
+	return new Promise(function(resolve, reject) {
+		request.get(options, (err, res, body) => {
+			if (!err && res.statusCode === 200) {
+				resolve(body);
+			} else {
+				reject(err);
+			}
+		})
+	});
+}
+
+const twitch = {
+	getVODS: async () => {
+		return new Promise((resolve, reject) => {
+			getToken(async (at) => {
+				let pagination = undefined;
+				const res = [];
+				do {
+					const r = await getVODPage(at, pagination);
+					res.push(...r.data);
+					pagination = r.pagination.cursor;
+				}
+				while(pagination)
+				resolve(res.reverse());
+			});
+		});
+	},
+
+	getClips: async () => {
+		return new Promise((resolve, reject) => {
+			getToken(async (at) => {
+				let pagination = undefined;
+				const res = [];
+				do {
+					const r = await getClipsPage(at, pagination);
+					res.push(...r.data);
+					pagination = r.pagination.cursor;
+				}
+				while(pagination)
+				resolve(res.reverse());
+			});
+		});
+	},
+
+	getChannel: async () => {
+		return new Promise((resolve, reject) => {
+			getToken(async (at) => {
+				const aStreamers = await getStreamers(at);
+				const stream = aStreamers.data.filter(s => s.broadcaster_login === config["BROADCASTER"]["NAME"])[0];
+				resolve(stream);
+			});
+		});
+	},
+
+	getGame: async (id) => {
+		return new Promise((resolve, reject) => {
+			getToken(async (at) => {
+				const games = await getGames(at, id);
+				resolve(games.data[0]);
+			});
+		});
+	},
+
+	getUser: async (id) => {
+		return new Promise((resolve, reject) => {
+			getToken(async (at) => {
+				const users = await getUsers(at, id);
+				resolve(users.data[0]);
+			});
+		});
+	},
+}
+
+module.exports = twitch;
